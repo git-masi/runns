88f42bd0da9a9b7a41c00a32822ad15252329ab7
Eric Masi <emasi@vitusvet.com>
2023-08-11 10:54:57 -0400

    Move dir
    Make the prompt more readable by using a sub directory and excluding
    files like index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { URL, fileURLToPath } from "node:url";
 import inquirer from "inquirer";
 import { getChoicesFromDir } from "./file-system.js";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const choices = await getChoicesFromDir(__dirname);
+const scriptsDir = fileURLToPath(new URL("./scripts/", import.meta.url));
+const choices = await getChoicesFromDir(scriptsDir);
 
 const selectedScripts = await inquirer.prompt([
   {
